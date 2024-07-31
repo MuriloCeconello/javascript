98b4cb01eea200d8b4ff96b71ff7a590b93b1105
MuriloCeconello <ceconellomurilo@gmail.com>
2024-07-31 11:46:20 -0300

    create ambiente.js

---
+++ exercicios/ex007-aula13/ambiente.js
@@ -0,0 +1,16 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+
+
+
+/*var contador = 1
+
+while (contador <= 10) {
+    console.log(`Passo ${contador}`)
+    contador++
+}*/ 
