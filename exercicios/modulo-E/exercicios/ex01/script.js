a89a42f360dd80d12904e047d5e2b9681134b73b
MuriloCeconello <ceconellomurilo@gmail.com>
2024-07-31 21:25:41 -0300

    update script.js

--- exercicios/modulo-E/exercicios/ex01/script.js
+++ exercicios/modulo-E/exercicios/ex01/script.js
@@ -3,9 +3,13 @@ var fim = document.getElementById('txtfim')
 var passo = document.getElementById('txtpasso')
 var res = document.getElementById('res')
 var emojipas = String.fromCharCode('U+1F449')
+var emojifim = String.fromCharCode('🏁')
 
-while(inicio <= fim){
-    res.innerHTML = `${inicio}`
-    res.innerText += `${emojipas}`
-    passo++ 
+function contar() {
+    while (inicio <= fim) {
+        res.innerHTML = `${inicio} ${emojipas} ${emojifim}`
+        inicio+=passo
+        emojipas++
+    }
 }
+
